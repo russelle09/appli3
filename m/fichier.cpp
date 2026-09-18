0597daaa85a765e0d91bc3d07c95f0cf9e9f7f64
MKRUSSELLE-e <ngamaleumonkamrussellevire@gmail.com>
2026-09-18 23:18:55 +0100

    ajout de la fonction calculer
    j'ai ajoutee la fonction pour mener des operations sur les nombres entres par l'utilisateur

--- m/fichier.cpp
+++ m/fichier.cpp
@@ -23,5 +23,6 @@ int main() {
     std::cout<<"le resultat de la multiplication est :"<<x*a;
     std::cout<<"le resultat de la division est :"<<x/b;
     std::cout<<" votre programme se termine ici";
+    int calculer(int x,int a,int b);
     return 0;
    }
